b5353fe5e43fc17458eb00e171fc0ac9fc92839d
Ankur Bhatkalkar <ankurbhatkalkar@pop-os.localdomain>
2023-10-11 23:02:40 -0400

    Got the JSON parser working up until step 4.

--- jparser/jparser.py
+++ jparser/jparser.py
@@ -12,36 +12,54 @@ async def tokenizer(string: str) -> List[str]:
         return ""
     while i < len(string):
         # print(i)
-        if string[i] == "{" or string[i] == "}" or  string[i] == ":" or string[i]==",":
+        if (
+            string[i] == "{"
+            or string[i] == "}"
+            or string[i] == ":"
+            or string[i] == ","
+            or string[i] == "["
+            or string[i] == "]"
+        ):
             output.append(string[i])
-            i+=1
+            i += 1
             continue
         elif string[i] == '"':
             temp = '"'
-            i+=1
+            i += 1
             while string[i] != '"':
                 temp += string[i]
-                i+=1
+                i += 1
             temp += string[i]
-            i+=1
+            i += 1
             output.append(temp)
             continue
         elif string[i] == "\n" or string[i] == " ":
-            i+=1
+            i += 1
             continue
         else:
-            temp=""
-            while string[i] != "}" and string[i] != "," and string[i] != "\n" and string!=" ":
+            temp = ""
+            while (
+                string[i] != "}"
+                and string[i] != ","
+                and string[i] != "\n"
+                and string != " "
+            ):
+                if (
+                    string[i] == "'"
+                ):  # We cannot allow single quotes in if it's a string type
+                    print("Invalid string identifier")
+                    sys.exit(1)
                 temp += string[i]
-                i+=1
+                i += 1
             output.append(temp)
             continue
     # print(output)
     return output
 
-async def determine_value_type(string:str)-> int | bool | str | float | None :
+
+async def determine_value_type(string: str) -> int | bool | str | float | None:
     if '"' in string:
-        return string.replace('"','')
+        return string.replace('"', "")
     if string == "true":
         return True
     if string == "false":
@@ -52,33 +70,76 @@ async def determine_value_type(string:str)-> int | bool | str | float | None :
         return int(string)
     if string.isdecimal():
         return float(string)
-    
+
     print("Invalid value {}".format(string))
     sys.exit(1)
 
-async def analyse_syntax(tokens:List):
+
+async def analyse_syntax(tokens: List):
     current_object = None
     token_stack = []
     i = 0
     # print(tokens)
     while i < len(tokens):
         # print(token_stack)
-        if tokens[i] == "{":
+        if tokens[i] == "[":
+            token_stack.append(tokens[i])
+            current_object = []
+        elif tokens[i] == "{":
             token_stack.append(tokens[i])
             current_object = {}
         elif tokens[i] == ":":
-            i+=1
+            i += 1
             if tokens[i] == ",":
                 print("Invalid JSON format, expected key value got empty")
                 sys.exit(1)
             if token_stack[-1] == ",":
                 print("Missing key for value")
                 sys.exit(1)
-            if tokens[i]:
-                current_object[token_stack.pop(-1)] = await determine_value_type(tokens[i])
+            if tokens[i] == "{":
+                temp_array = []
+                while not tokens[i] == "}":
+                    temp_array.append(tokens[i])
+                    i += 1
+                # double check the value of the token ender is indeed the } bracket
+                if tokens[i] == "}":
+                    temp_array.append(tokens[i])
+                    if isinstance(current_object, dict):
+                        current_object[token_stack.pop(-1)] = await analyse_syntax(
+                            temp_array
+                        )
+                    elif isinstance(current_object, list):
+                        current_object.append(await analyse_syntax(temp_array))
+                    if token_stack[-1] == ",":
+                        token_stack.pop(-1)
+            elif tokens[i] == "[":
+                temp_array = []
+                while not tokens[i] == "]":
+                    temp_array.append(tokens[i])
+                    i += 1
+                # double check the value of the token ender is indeed the } bracket
+                if tokens[i] == "]":
+                    temp_array.append(tokens[i])
+                    if isinstance(current_object, dict):
+                        current_object[token_stack.pop(-1)] = await analyse_syntax(
+                            temp_array
+                        )
+                    elif isinstance(current_object, list):
+                        current_object.append(await analyse_syntax(temp_array))
+                    if token_stack[-1] == ",":
+                        token_stack.pop(-1)
+            elif tokens[i]:
+                if isinstance(current_object, dict):
+                    current_object[token_stack.pop(-1)] = await determine_value_type(
+                        tokens[i]
+                    )
+                elif isinstance(current_object, list):
+                    current_object.append(
+                        await determine_value_type(token_stack.pop(-1))
+                    )
                 # If a key value gets added then we should also see that the token stack will probably have a comma value
                 if token_stack[-1] == ",":
-                    #If so we can safely pop this
+                    # If so we can safely pop this
                     token_stack.pop(-1)
         elif tokens[i] == "}":
             # We should ideally be left with only 1 value here
@@ -92,15 +153,28 @@ async def analyse_syntax(tokens:List):
                 sys.exit(1)
         elif tokens[i] == ",":
             token_stack.append(tokens[i])
+        elif tokens[i] == "]":
+            # we need to ensure that there's no trailing comma
+            if token_stack[-1] == ",":
+                print("Extra comma before array entry")
+                sys.exit(1)
+            if token_stack[-1] == "[":
+                token_stack.pop(-1)
         else:
             if tokens[i][0] == '"' and tokens[i][-1] == '"' and len(tokens[i]) > 2:
-                token_stack.append(tokens[i].replace('"',''))
+                if isinstance(current_object, list):
+                    current_object.append(tokens[i].replace('"', ""))
+                elif isinstance(current_object, dict):
+                    token_stack.append(tokens[i].replace('"', ""))
             else:
                 print("Invalid key value")
                 sys.exit(1)
-        i+=1
-    return current_object if current_object else ""
-            
+        i += 1
+    # if len(token_stack) > 0:
+    #     print("Invalid JSON")
+    # sys.exit(1)
+    return current_object
+
 
 async def main():
     # print(os.getcwd())
